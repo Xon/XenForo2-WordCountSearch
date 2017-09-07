55e524834f531eb3bf221e8ccbb5f2367c4522f6
Xon <tazipper1@hotmail.com>
2017-09-07 15:44:18 +0800

    Push range query into ElasticSearch

--- upload/src/addons/SV/WordCountSearch/XFES/Search/Source/Elasticsearch.php
+++ upload/src/addons/SV/WordCountSearch/XFES/Search/Source/Elasticsearch.php
@@ -2,6 +2,7 @@
 
 namespace SV\WordCountSearch\XFES\Search\Source;
 
+use SV\WordCountSearch\XF\Search\Query\RangeMetadataConstraint;
 use XF\Search\Query;
 
 class Elasticsearch extends XFCP_Elasticsearch
@@ -25,5 +26,40 @@ class Elasticsearch extends XFCP_Elasticsearch
         }
 
         return $dsl;
+    }
+
+    protected function applyMetadataConstraint(Query\MetadataConstraint $metadata, array &$filters, array &$filtersNot)
+    {
+        if ($metadata instanceof RangeMetadataConstraint)
+        {
+            $values = $metadata->getValues();
+
+            switch ($metadata->getMatchType())
+            {
+                case RangeMetadataConstraint::MATCH_LESSER:
+                    $filters[] = [
+                        'range' => [$metadata->getKey() => [
+                            "lte" => $values[0],
+                        ]]
+                    ];
+                    return;
+                case RangeMetadataConstraint::MATCH_GREATER:
+                    $filters[] = [
+                        'range' => [$metadata->getKey() => [
+                            "gte" => $values[0],
+                        ]]
+                    ];
+                    return;
+                case RangeMetadataConstraint::MATCH_BETWEEN:
+                    $filters[] = [
+                        'range' => [$metadata->getKey() => [
+                            "lte" => $values[0],
+                            "gte" => $values[1],
+                        ]]
+                    ];
+                    return;
+            }
+        }
+        parent::applyMetadataConstraint($metadata, $filters, $filtersNo);
     }
 }
