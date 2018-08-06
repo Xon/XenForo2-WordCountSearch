34089dfa4d985df9b9e4c6226f4d83b0bb024313
Xon <tazipper1@hotmail.com>
2018-08-07 01:33:21 +0800

    Rework installer to standard pattern

--- upload/src/addons/SV/WordCountSearch/Setup.php
+++ upload/src/addons/SV/WordCountSearch/Setup.php
@@ -24,53 +24,133 @@ class Setup extends AbstractSetup
     {
         $sm = $this->schemaManager();
 
-        $sm->createTable(
-            'xf_post_words',
-            function (Create $table) {
-                $table->addColumn('post_id','int')->primaryKey();
-                $table->addColumn('word_count', 'int');
-            }
-        );
+        foreach ($this->getTables() as $tableName => $callback)
+        {
+            $sm->createTable($tableName, $callback);
+            $sm->alterTable($tableName, $callback);
+        }
+    }
 
-        $sm->alterTable(
-            'xf_search_index',
-            function (Alter $table) {
-                $table->addColumn('word_count', 'int')->nullable(true);
-            }
-        );
+    public function installStep2()
+    {
+        $sm = $this->schemaManager();
+
+        foreach ($this->getAlterTables() as $tableName => $callback)
+        {
+            $sm->alterTable($tableName, $callback);
+        }
     }
 
     public function upgrade2010000Step1()
     {
-        $sm = $this->schemaManager();
+        $this->installStep1();
+    }
 
-        $sm->alterTable('xf_thread', function (Alter $table)
-        {
-            $table->addColumn('word_count', 'int')->nullable(true)->setDefault(null);
-        });
+    public function upgrade2010000Step2()
+    {
+        $this->installStep2();
     }
 
     public function uninstallStep1()
     {
         $sm = $this->schemaManager();
 
-        $sm->dropTable('xf_post_words');
-
-        $sm->alterTable(
-            'xf_search_index',
-            function (Alter $table) {
-                $table->dropColumns(['word_count']);
-            }
-        );
+        foreach ($this->getTables() as $tableName => $callback)
+        {
+            $sm->dropTable($tableName);
+        }
     }
 
     public function uninstallStep2()
     {
         $sm = $this->schemaManager();
 
-        $sm->alterTable('xf_thread', function (Alter $table)
+        foreach ($this->getRemoveAlterTables() as $tableName => $callback)
         {
-            $table->dropColumns('word_count');
-        });
+            $sm->alterTable($tableName, $callback);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTables()
+    {
+        $tables = [];
+
+        $tables['xf_post_words'] = function ($table) {
+            /** @var Create|Alter $table */
+            $this->addOrChangeColumn($table,'post_id','int')->primaryKey();
+            $this->addOrChangeColumn($table,'word_count', 'int');
+        };
+
+        return $tables;
+    };
+
+    /**
+     * @return array
+     */
+    protected function getAlterTables()
+    {
+        $tables = [];
+
+        $tables['xf_thread'] = function (Alter $table) {
+            $this->addOrChangeColumn($table,'word_count', 'int')->nullable(true)->setDefault(null);
+        };
+
+        $tables['xf_search_index'] = function (Alter $table) {
+            $this->addOrChangeColumn($table,'word_count', 'int')->nullable(true)->setDefault(null);
+        };
+
+        return $tables;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getRemoveAlterTables()
+    {
+        $tables = [];
+
+        $tables['xf_thread'] = function (Alter $table) {
+            $table->dropColumns(['word_count']);
+        };
+
+        $tables['xf_search_index'] = function (Alter $table) {
+            $table->dropColumns(['word_count']);
+        };
+
+        return $tables;
+    }
+
+    /**
+     * @param Create|Alter $table
+     * @param string       $name
+     * @param string|null  $type
+     * @param string|null  $length
+     * @return \XF\Db\Schema\Column
+     */
+    protected function addOrChangeColumn($table, $name, $type = null, $length = null)
+    {
+        if ($table instanceof Create)
+        {
+            $table->checkExists(true);
+
+            return $table->addColumn($name, $type, $length);
+        }
+        else if ($table instanceof Alter)
+        {
+            if ($table->getColumnDefinition($name))
+            {
+                return $table->changeColumn($name, $type, $length);
+            }
+
+            return $table->addColumn($name, $type, $length);
+        }
+        else
+        {
+            throw new \LogicException("Unknown schema DDL type ". get_class($table));
+
+        }
     }
 }
