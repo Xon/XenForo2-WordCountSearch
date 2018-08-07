16d0bec5446b789364232e430d158e1514ee7b75
ticktackk <ims3kar@gmail.com>
2018-08-07 06:59:29 +0530

    Hook into getThreadmarkCategoryData() method to include word count

---
+++ upload/src/addons/SV/WordCountSearch/SV/Threadmarks/Repository/ThreadmarkCategory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SV\WordCountSearch\SV\Threadmarks\Repository;
+
+use XF\Mvc\Entity\Entity;
+
+/**
+ * Class ThreadmarkCategory
+ *
+ * @package SV\WordCountSearch\SV\Threadmarks\Repository
+ */
+class ThreadmarkCategory extends XFCP_ThreadmarkCategory
+{
+    /**
+     * @param Entity $container
+     *
+     * @return array
+     */
+    public function getThreadmarkCategoryData(Entity $container)
+    {
+        if (!$container instanceof \XF\Entity\Thread)
+        {
+            return parent::getThreadmarkCategoryData($container);
+        }
+
+        $db = $this->db();
+
+        return $db->fetchAllKeyed('
+            SELECT threadmark.threadmark_category_id, MAX(threadmark.position) AS position, SUM(COALESCE(post_word.word_count, 0)) AS word_count
+            FROM xf_sv_threadmark AS threadmark
+            LEFT JOIN xf_post_words AS post_word ON (threadmark.content_id = post_word.post_id AND threadmark.content_type = ?)
+            WHERE threadmark.container_id = ?
+              AND threadmark.container_type = ?
+            GROUP BY threadmark.threadmark_category_id
+            ORDER BY threadmark.threadmark_category_id ASC
+        ', 'threadmark_category_id', ['post', $container->getEntityId(), $container->getEntityContentType()]);
+    }
+}
--- upload/src/addons/SV/WordCountSearch/_output/extension_hint.php
+++ upload/src/addons/SV/WordCountSearch/_output/extension_hint.php
@@ -3,6 +3,11 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
+namespace SV\WordCountSearch\SV\Threadmarks\Repository
+{
+	class XFCP_ThreadmarkCategory extends \SV\Threadmarks\Repository\ThreadmarkCategory {}
+}
+
 namespace SV\WordCountSearch\XFES\Search\Source
 {
 	class XFCP_Elasticsearch extends \XFES\Search\Source\Elasticsearch {}
