23f3db05ebfe36ffdd383b57d632eed8ed851457
Xon <tazipper1@hotmail.com>
2019-07-19 09:15:25 +0800

    Add CLI tools for rebuilding post/thread word-counts

---
+++ upload/src/addons/SV/WordCountSearch/Cli/Command/Rebuild/PostWordCount.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SV\Threadmarks\Cli\Command\Rebuild;
+
+use Symfony\Component\Console\Input\InputOption;
+use XF\Cli\Command\Rebuild\AbstractRebuildCommand;
+
+class PostWordCount extends AbstractRebuildCommand
+{
+	protected function getRebuildName()
+	{
+		return 'word-count-post';
+	}
+
+	protected function getRebuildDescription()
+	{
+		return 'Rebuild post word counts';
+	}
+
+	protected function getRebuildClass()
+	{
+		return 'SV\WordCountSearch:PostWordCount';
+	}
+
+	protected function configureOptions()
+	{
+	}
+}
---
+++ upload/src/addons/SV/WordCountSearch/Cli/Command/Rebuild/ThreadWordCount.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SV\Threadmarks\Cli\Command\Rebuild;
+
+use Symfony\Component\Console\Input\InputOption;
+use XF\Cli\Command\Rebuild\AbstractRebuildCommand;
+
+class ThreadWordCount extends AbstractRebuildCommand
+{
+	protected function getRebuildName()
+	{
+		return 'word-count-thread';
+	}
+
+	protected function getRebuildDescription()
+	{
+		return 'Rebuild thread word counts';
+	}
+
+	protected function getRebuildClass()
+	{
+		return 'SV\WordCountSearch:ThreadWordCount';
+	}
+
+	protected function configureOptions()
+	{
+	}
+}
