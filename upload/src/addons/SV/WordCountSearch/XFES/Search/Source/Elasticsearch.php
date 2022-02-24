0775db82c285b172eea183a023a6e624a531d2fe
Xon <tazipper1@hotmail.com>
2022-02-24 09:59:11 +0800

    Remove XF2.1 support

--- upload/src/addons/SV/WordCountSearch/XFES/Search/Source/Elasticsearch.php
+++ upload/src/addons/SV/WordCountSearch/XFES/Search/Source/Elasticsearch.php
@@ -1,31 +1,18 @@
 <?php
+/**
+ * @noinspection PhpMissingReturnTypeInspection
+ */
 
 namespace SV\WordCountSearch\XFES\Search\Source;
 
 use XF\Search\Query;
+use function strpos;
 
 /**
  * Extends \XFES\Search\Source\Elasticsearch
  */
 class Elasticsearch extends XFCP_Elasticsearch
 {
-    /**
-     * XF2.0/XF2.1 support
-     *
-     * @param Query\Query $query
-     * @param int         $maxResults
-     * @return array
-     * @noinspection PhpMissingParamTypeInspection
-     */
-    protected function getDslFromQuery(Query\Query $query, $maxResults)
-    {
-        /** @noinspection PhpUndefinedMethodInspection */
-        $dsl = parent::getDslFromQuery($query, $maxResults);
-        $dsl = $this->rewriteDslForWordCountOrder($query, $dsl);
-
-        return $dsl;
-    }
-
     protected function rewriteDslForWordCountOrder(Query\Query $query, array $dsl)
     {
         // rewrite order-by since getDslFromQuery is inflexible
@@ -43,12 +30,9 @@ class Elasticsearch extends XFCP_Elasticsearch
     }
 
     /**
-     * XF2.2+ support
-     *
      * @param Query\KeywordQuery $query
      * @param int                $maxResults
      * @return array
-     * @noinspection PhpMissingParamTypeInspection
      */
     public function getKeywordSearchDsl(Query\KeywordQuery $query, $maxResults)
     {
