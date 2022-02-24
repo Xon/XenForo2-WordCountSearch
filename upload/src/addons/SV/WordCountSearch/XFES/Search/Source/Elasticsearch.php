c50432cda0a44842c95a7c45aea3be899cce64d8
Xon <tazipper1@hotmail.com>
2022-02-24 10:05:38 +0800

    Use XFES2.2+ native getSearchSortDsl function

--- upload/src/addons/SV/WordCountSearch/XFES/Search/Source/Elasticsearch.php
+++ upload/src/addons/SV/WordCountSearch/XFES/Search/Source/Elasticsearch.php
@@ -6,39 +6,28 @@
 namespace SV\WordCountSearch\XFES\Search\Source;
 
 use XF\Search\Query;
-use function strpos;
 
 /**
  * Extends \XFES\Search\Source\Elasticsearch
  */
 class Elasticsearch extends XFCP_Elasticsearch
 {
-    protected function rewriteDslForWordCountOrder(Query\Query $query, array $dsl)
+    /**
+     * @param Query\Query $query
+     * @return array
+     */
+    protected function getSearchSortDsl(Query\Query $query)
     {
-        // rewrite order-by since getDslFromQuery is inflexible
         $orderByClause = $query->getOrder();
         if ($orderByClause instanceof Query\SqlOrder &&
-            strpos($orderByClause->getOrder(), 'search_index.word_count') === 0)
+            $orderByClause->getOrder() === 'search_index.word_count DESC')
         {
-            $query->orderedBy('date');
-            $dsl['sort'] = [
-                ['word_count' => 'desc']
+            return [
+                ['word_count' => 'desc'],
+                ['date' => 'desc'],
             ];
         }
 
-        return $dsl;
-    }
-
-    /**
-     * @param Query\KeywordQuery $query
-     * @param int                $maxResults
-     * @return array
-     */
-    public function getKeywordSearchDsl(Query\KeywordQuery $query, $maxResults)
-    {
-        $dsl = parent::getKeywordSearchDsl($query, $maxResults);
-        $dsl = $this->rewriteDslForWordCountOrder($query, $dsl);
-
-        return $dsl;
+        return parent::getSearchSortDsl($query);
     }
 }
