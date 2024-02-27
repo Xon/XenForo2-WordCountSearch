63cd546a4530c1acdf9faf019195b0b1e2a17654
Xon <635541+Xon@users.noreply.github.com>
2024-02-28 01:37:51 +0800

    version bump to trigger rebuild

--- upload/src/addons/SV/WordCountSearch/Setup.php
+++ upload/src/addons/SV/WordCountSearch/Setup.php
@@ -51,12 +51,12 @@ class Setup extends AbstractSetup
         $this->applyLegacySchemaUpdates();
     }
 
-    public function upgrade1709054097Step1(): void
+    public function upgrade1709054098Step1(): void
     {
         $this->applySchemaUpdates();
     }
 
-    public function upgrade1709054097Step2(array $stepParams): ?array
+    public function upgrade1709054098Step2(array $stepParams): ?array
     {
         $position = $stepParams[0] ?? 0;
         $stepData = $stepParams[2] ?? [];
