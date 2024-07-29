d166564de2e8df242feb121fd00abae83b40544b
Xon <635541+Xon@users.noreply.github.com>
2024-07-30 01:18:10 +0800

    Generate finder phpdocs

--- upload/src/addons/SV/WordCountSearch/Finder/PostWords.php
+++ upload/src/addons/SV/WordCountSearch/Finder/PostWords.php
@@ -4,7 +4,15 @@ namespace SV\WordCountSearch\Finder;
 
 use SV\StandardLib\Helper;
 use XF\Mvc\Entity\Finder;
+use XF\Mvc\Entity\AbstractCollection as AbstractCollection;
+use SV\WordCountSearch\Entity\PostWords as PostWordsEntity;
 
+/**
+ * @method AbstractCollection<PostWordsEntity>|PostWordsEntity[] fetch(?int $limit = null, ?int $offset = null)
+ * @method PostWordsEntity|null fetchOne(?int $offset = null)
+ * @implements \IteratorAggregate<string|int,PostWordsEntity>
+ * @extends Finder<PostWordsEntity>
+ */
 class PostWords extends Finder
 {
     public static function finder(): self
