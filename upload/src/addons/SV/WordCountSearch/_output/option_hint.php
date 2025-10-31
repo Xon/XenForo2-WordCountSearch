182e69beaa0863feb8a5c7dc2426c52870d74afc
Xon <635541+Xon@users.noreply.github.com>
2025-11-01 03:02:00 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/WordCountSearch/_output/option_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property positive-int|null $wordcountThreshold Word Count threshold
+ */
+class Options
+{
+}
